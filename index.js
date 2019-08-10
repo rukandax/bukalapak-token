a0ec0e820336ad00dc47b0e0f86356fcd9b566db
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2019-08-10 16:45:32 +0700

    Push Code

---
+++ index.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', async (_, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.bukalapak.com');
+
+  const token = await page.evaluate(() => {
+    return document.querySelector("meta[name='oauth-access-token']").getAttribute("content");
+  });
+
+  await browser.close();
+
+  return res.send(token);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
