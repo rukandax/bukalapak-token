38e906ca9b8b1f9ca203a03d38573c55049fe7f8
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2019-08-10 19:06:18 +0700

    Update express callback text

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ app.get('/', async (_, res) => {
   return res.send(token);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`bukalapak-token listening on port ${port}!`));
